8e7f22273e685d7dc4fdfcbda685294f2a59d24a
brizzigui <brizzigui@gmail.com>
2023-04-26 23:18:51 -0300

    Adiciona script inicial

---
+++ script_renovador.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+import time
+from time import gmtime, strftime
+
+import getpass
+
+usuario = input("Insira seu usuário: ")
+senha = getpass.getpass("Insira sua senha: ")
+numero_de_livros = int(input("Insira o número de livros: "))
+
+
+driver = webdriver.Chrome()
+driver.get("https://portal.ufsm.br/biblioteca/login.html")
+
+campo_usuario = driver.find_element(By.XPATH, '//*[@id="login"]')
+campo_senha = driver.find_element(By.XPATH, '//*[@id="senha"]')
+
+campo_usuario.send_keys(f"{usuario}")
+campo_senha.send_keys(f"{senha}")
+campo_senha.send_keys(Keys.RETURN)
+
+pagina_renovacao = driver.find_element(By.XPATH, '//*[@id="app_top"]/nav/div/ul/li[4]/a')
+pagina_renovacao.click()
+
+data_atual = strftime("%d/%m/%Y", time.localtime())
+
+
+for i in range(numero_de_livros):
+
+    data_limite = driver.find_element(By.XPATH, f'//*[@id="emprestimos"]/tbody/tr[{i+1}]/td[6]').text
+
+    print(f"Data limite: {data_limite}")
+    print(f"Data atual: {data_atual}")
+
+    if data_atual == data_limite:
+        botao_renovacao = driver.find_element(By.XPATH, f'//*[@id="emprestimos"]/tbody/tr[{i+1}]/td[1]/button')
+        botao_renovacao.click()
+
+input("ENTER")
