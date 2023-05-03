43e23a654ad580bd3086c8234dbb7226dfa06dd0
Guilherme Brizzi <brizzigui@gmail.com>
2023-05-02 22:52:28 -0300

    Update script_renovador.py

--- script_renovador.py
+++ script_renovador.py
@@ -23,7 +23,7 @@ chrome_options.add_argument('start-maximized')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_options.binary_location('/opt/hostedtoolcache/chromium/latest/x64/chrome')
+chrome_options.binary_location = "/opt/hostedtoolcache/chromium/latest/x64/chrome"
 driver = webdriver.Chrome(chrome_options=chrome_options)
 driver.get("https://portal.ufsm.br/biblioteca/login.html")
 
