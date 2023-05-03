f3030fd0ca9bc00d64f74517eb2d861927f4d6ff
Guilherme Brizzi <brizzigui@gmail.com>
2023-05-02 22:24:07 -0300

    Update script_renovador.py

--- script_renovador.py
+++ script_renovador.py
@@ -19,6 +19,7 @@ chrome_options = Options()
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--disable-dev-shm-usage")
 driver = webdriver.Chrome(chrome_options=chrome_options)
 driver.get("https://portal.ufsm.br/biblioteca/login.html")
 
