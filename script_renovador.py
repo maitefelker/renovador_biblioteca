a74bc525a8d557f2c2729dea76528a31f6994178
Guilherme Brizzi <brizzigui@gmail.com>
2023-05-02 22:27:55 -0300

    Update script_renovador.py

--- script_renovador.py
+++ script_renovador.py
@@ -16,10 +16,14 @@ senha = os.environ['SENHA']
 
 
 chrome_options = Options()
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-setuid-sandbox')
+chrome_options.add_argument('--remote-debugging-port=9222')
+chrome_options.add_argument('--disable-extensions')
+chrome_options.add_argument('start-maximized')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(chrome_options=chrome_options)
 driver.get("https://portal.ufsm.br/biblioteca/login.html")
 
